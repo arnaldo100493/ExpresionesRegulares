d713d820b35181d372439bbd934c78d688689947
Arnaldo Andrés Barrios Mena <proveedor_shernandez@ath.com.co>
2019-11-25 21:23:39 -0500

    Subida de proyecto a repositorio Completo.

---
+++ ExpresionesRegulares/src/Main.java
@@ -0,0 +1,23 @@
+
+import paquete.FormularioPrincipal;
+
+/**
+ *
+ * @author abarrime
+ */
+public class Main {
+
+    public Main() {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormularioPrincipal ventana = new FormularioPrincipal();
+        ventana.setVisible(true);
+    }
+
+}
